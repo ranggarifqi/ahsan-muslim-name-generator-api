3fd88187bac69321e1de62459f57e554fc36fa2e
Rangga Rifqi Pratama <rangga@ranggarifqi.com>
2021-06-25 22:00:14 +0700

    Implement user FindById usecase

---
+++ src/user/usecase/user_usecase.go
@@ -0,0 +1,35 @@
+package usecase
+
+import "github.com/ranggarifqi/ahsan-muslim-name-generator-api/src/user"
+
+type userUsecase struct {
+	ur user.IUserRepository
+}
+
+func NewUserUsecase(ur user.IUserRepository) user.IUserUsecase {
+	return &userUsecase{
+		ur,
+	}
+}
+
+func (uuc *userUsecase) FindById(id string) (*user.UserWithoutPassword, error) {
+	u, err := uuc.ur.FindById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &user.UserWithoutPassword{
+		ID:        u.ID,
+		Email:     u.Email,
+		FullName:  u.FullName,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+		DeletedAt: u.DeletedAt,
+	}
+
+	return res, nil
+}
+
+func (uuc *userUsecase) SignIn(payload *user.SignInDTO) (*user.SignInResult, error) {
+	return nil, nil
+}
