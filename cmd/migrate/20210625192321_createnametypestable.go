c54eff1f0fefe45bf657f4d1bccd933eb3c9e343
Rangga Rifqi Pratama <rangga@ranggarifqi.com>
2021-06-25 19:27:14 +0700

    Migration: Create table nameTypes

---
+++ cmd/migrate/20210625192321_createnametypestable.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/pressly/goose"
+	"github.com/ranggarifqi/ahsan-muslim-name-generator-api/helper"
+)
+
+func init() {
+	goose.AddMigration(upCreatenametypestable, downCreatenametypestable)
+}
+
+func upCreatenametypestable(tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	query := `CREATE TABLE IF NOT EXISTS nameTypes (
+		id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+		name VARCHAR(255) NOT NULL,
+		createdBy uuid NULL,
+		updatedBy uuid NULL,
+		createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		deletedAt TIMESTAMP NULL
+	)`
+
+	_, err := tx.Exec(query)
+	helper.HandleError("Error creating nameTypes table", err)
+	return nil
+}
+
+func downCreatenametypestable(tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	_, err := tx.Exec("DROP TABLE IF EXISTS nameTypes")
+	helper.HandleError("Error when dropping table nameTypes", err)
+	return nil
+}
