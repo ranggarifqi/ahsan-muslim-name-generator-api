4f93634efba1a96c25fa15611f57db2f0adc8e0a
Rangga Rifqi Pratama <rangga@ranggarifqi.com>
2021-06-26 20:20:33 +0700

    Ties user instance on main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/ranggarifqi/ahsan-muslim-name-generator-api/database/postgresql"
+	userH "github.com/ranggarifqi/ahsan-muslim-name-generator-api/src/user/handler"
+	userRepo "github.com/ranggarifqi/ahsan-muslim-name-generator-api/src/user/repository"
+	userUC "github.com/ranggarifqi/ahsan-muslim-name-generator-api/src/user/usecase"
 	myValidator "github.com/ranggarifqi/ahsan-muslim-name-generator-api/src/validator"
 )
 
@@ -21,17 +26,21 @@ func main() {
 
 	e.Validator = myValidator.NewMyValidator()
 
-	// db := postgresql.InitDB()
+	db := postgresql.InitDB()
 
-	// e.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello World")
-	// })
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
 
-	// v1Group := e.Group("/api/v1")
+	v1Group := e.Group("/api/v1")
 
 	// urlRepository := urlRepo.NewUrlRepository(db)
 	// urlUsecase := urlUC.NewUrlUsecase(urlRepository)
 	// urlH.NewUrlHandler(v1Group, urlUsecase)
+
+	userRepository := userRepo.NewUserRepository(db)
+	userUsecase := userUC.NewUserUsecase(userRepository)
+	userH.NewUserHandler(v1Group, userUsecase)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", os.Getenv("PORT"))))
 }
